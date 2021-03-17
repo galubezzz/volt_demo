7b89c860959d86ea3baaf5f988329cc49c37e536
galubezzz <galubezzz@gmail.com>
2021-03-17 17:06:48 -0500

    add filter by required equipment

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,5 +2,9 @@ class ProgramsController < ApplicationController
   def index
     @programs = Program.includes(:sport, :required_equipment).all
     @programs = @programs.where(sport_id: params[:sport_id]) if params[:sport_id]
+    if params[:equipment_ids]
+      @programs = @programs.joins(:equipment_requirements).merge(EquipmentRequirement.where(equipment_id: params[:equipment_ids])).distinct 
+      @programs = @programs.to_a.reject{|program| (program.required_equipment_ids - Array(params[:equipment_ids]).map{|x| x.to_i}).length > 0}
+    end
   end
 end
