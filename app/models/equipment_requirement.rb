9feb5cce82eb490904e16cf5ccd885192d9e6865
galubezzz <galubezzz@gmail.com>
2021-03-17 15:13:52 -0500

    validate program required equipment uniqueness

--- app/models/equipment_requirement.rb
+++ app/models/equipment_requirement.rb
@@ -1,4 +1,5 @@
 class EquipmentRequirement < ApplicationRecord
   belongs_to :program, inverse_of: :equipment_requirements
   belongs_to :equipment, inverse_of: :equipment_requirements
+  validates :equipment, uniqueness: {scope: :program}
 end
