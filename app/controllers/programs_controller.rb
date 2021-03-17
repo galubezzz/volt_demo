2b012ec8e1a6db7b947f2bff9caecf6b4c4ed278
galubezzz <galubezzz@gmail.com>
2021-03-17 16:13:18 -0500

    add filter by sport

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,6 @@
 class ProgramsController < ApplicationController
   def index
     @programs = Program.includes(:sport, :required_equipment).all
+    @programs = @programs.where(sport_id: params[:sport_id]) if params[:sport_id]
   end
 end
